00d6c39c398d371cb6341ec7f9b5ea5de2517dbd
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2020-12-22 18:28:00 +0700

    crud with mongoose odm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+  ],
   controllers: [AppController, CoffeesController],
   providers: [AppService, CoffeesService],
 })
---
+++ src/events/entities/event.entity.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+@Schema()
+export class Event extends mongoose.Document {
+  // Note "entity" was removed from the class "name"
+  @Prop()
+  type: string;
+
+  @Prop({ index: true })
+  name: string;
+
+  @Prop({ default: 0 })
+  recommendations: number;
+
+  @Prop(mongoose.SchemaTypes.Mixed)
+  payload: Record<string, any>;
+}
+
+export const EventSchema = SchemaFactory.createForClass(Event);
+
+EventSchema.index({ name: 1, type: -1 });
